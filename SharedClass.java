490b233756cb099ac8133851dd8d0aae629cd9a8
Jackffox12 <61726121+Jackffox12@users.noreply.github.com>
2020-07-12 15:05:27 -0600

    new version

--- SharedClass.java
+++ SharedClass.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.shared;
 
 public class SharedClass {
     public static void foo(){
-        System.out.println("this method is shared!");
+        System.out.println("this method is shared");
+
     }
 }
